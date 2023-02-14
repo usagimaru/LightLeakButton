575fe77066efa3c27b8421c49f4ab77e1e4a491c
usagimaru <usagimaruma@gmail.com>
2023-02-14 22:36:59 +0900

    Initial Commit

---
+++ LightLeakButton/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LightLeakButton
+//
+//  Created by usagimaru on 2023/02/14.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
